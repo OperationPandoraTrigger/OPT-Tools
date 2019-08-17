5bd5eb96745396bc51af40b02fbb399711d1c510
formtapez <github.com@posthub.de>
2019-08-17 13:05:30 +0200

    hardened clientStop script

--- tools/clientStop.bat
+++ tools/clientStop.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 ECHO ********************************************************
-ECHO *** ArmA stopper v0.11                               ***
+ECHO *** ArmA stopper v0.2                                ***
 ECHO *** This script stops a running local ArmA instance. ***
 ECHO ********************************************************
 
@@ -21,23 +21,31 @@ REM The loop occurs as long as the killing reports a success status as this mean
 REM there was a client process in the process table that was sent a kill-signal
 REM However this also means that the client is still running and as Windows doesn't really
 REM kill the program, we have to continously send the signal until the client is actually dead
+
 ECHO Killing the (potentially) running client. This might take a while...
-:killLoop
-	TASKKILL /IM %ArmaClientExe% > NUL 2>&1
-	
-	IF [%errorlevel%] == [0] (
-		REM sleep 100ms
-		CALL "%~dp0.\..\helpers\sleep.bat" 100
-		GOTO :killLoop
-	) ELSE (
-		REM wait another 100ms and check again if the client was _really_ (hard)killed
-		CALL "%~dp0.\..\helpers\sleep.bat" 100
-		TASKKILL /F /IM %ArmaClientExe% > NUL 2>&1
-		IF [%errorlevel%] == [0] (
-			REM apparently the client isn't as dead as it seemed
-			GOTO :killLoop
-		)
-	)
+:SoftKillLoop
+TASKKILL /IM %ArmaClientExe% > NUL 2>&1
+IF [%errorlevel%] == [0] (
+	REM sleep 100ms
+	CALL "%~dp0.\..\helpers\sleep.bat" 100
+	GOTO SoftKillLoop
+) ELSE (
+	:HardKillLoop
+	REM sleep 100ms
+	CALL "%~dp0.\..\helpers\sleep.bat" 100
+
+	REM Hardkill
+	TASKKILL /F /IM %ArmaClientExe% > NUL 2>&1
+
+	REM Task still active? -> Loop again
+	TASKLIST /FI "IMAGENAME EQ %ArmaClientExe%" 2> NUL | FIND /I /N "%ArmaClientExe%" > NUL
+	IF "%ERRORLEVEL%"=="0" GOTO HardKillLoop
+
+	REM Check (for reappearing ghost-process) again after short delay
+	CALL "%~dp0.\..\helpers\sleep.bat" 100
+	TASKLIST /FI "IMAGENAME EQ %ArmaClientExe%" 2> NUL | FIND /I /N "%ArmaClientExe%" > NUL
+	IF "%ERRORLEVEL%"=="0" GOTO HardKillLoop
+)
 ECHO.
 ECHO Successfully killed the client
 
