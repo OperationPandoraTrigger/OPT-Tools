8f936cfed6864bc0d2989c925a3e824228a8cc00
formtapez <github.com@posthub.de>
2022-03-06 09:17:02 +0100

    kill locking client after editing mission

--- tools/buildMission.bat
+++ tools/buildMission.bat
@@ -45,6 +45,28 @@ IF NOT EXIST "%ArmaMissionSourceDir%\%OptMissionName%" (
 	)
 )
 
+REM Kill the client
+REM The loop occurs as long as the killing reports a success status as this means
+REM there was a client process in the process table that was sent a kill-signal
+REM However this also means that the client is still running and as Windows doesn't really
+REM kill the program, we have to continously send the signal until the client is actually dead
+
+ECHO Killing the (potentially) running client. This might take a while...
+:HardKillLoop
+REM sleep 100ms
+CALL "%%~dp0.\..\helpers\sleep.bat" 100
+TASKKILL /F /IM %ArmaClientExe% > NUL 2>&1
+
+REM Task still active? -> Loop again
+TASKLIST /FI "IMAGENAME EQ %ArmaClientExe%" 2> NUL | FIND /I /N "%ArmaClientExe%" > NUL
+IF "%ERRORLEVEL%"=="0" GOTO HardKillLoop
+
+REM Check (for reappearing ghost-process) again after short delay
+CALL "%%~dp0.\..\helpers\sleep.bat" 100
+TASKLIST /FI "IMAGENAME EQ %ArmaClientExe%" 2> NUL | FIND /I /N "%ArmaClientExe%" > NUL
+IF "%ERRORLEVEL%"=="0" GOTO HardKillLoop
+ECHO Successfully killed the client
+
 REM Increase build-number (changes the #define in description.ext)
 ECHO Increasing build-number...
 FOR /F "TOKENS=3" %%a IN ('FINDSTR /B /C:"#define __VERSION__ " "%OptMissionRepoDir%\%OptMissionName%\description.ext"') DO (
